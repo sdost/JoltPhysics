80d967d2407406681cd6cfde14b78bdd037e2337
Samuel Dost <samuel.dost@gmail.com>
2024-02-22 13:53:04 -0500

    Add premake.

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+project "joltphysics"
+kind "StaticLib"
+language "C++"
+
+targetdir("bin/" .. outputdir .. "/%{prj.name}")
+objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+files
+{
+  "Jolt/**.h",
+  "Jolt/**.cpp",
+  "Jolt/**.inl"
+}
+
+includedirs
+{
+  "Jolt"
+}
+
+filter "system:windows"
+systemversion "latest"
+cppdialect "C++17"
+staticruntime "off"
+
+filter "system:linux"
+pic "On"
+systemversion "latest"
+cppdialect "C++17"
+staticruntime "off"
+
+filter "configurations:Debug"
+runtime "Debug"
+symbols "on"
+
+filter "configurations:Release"
+runtime "Release"
+optimize "on"
